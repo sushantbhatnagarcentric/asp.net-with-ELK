22a6c1ac5a570c689af8245afd4ccf92ef42309b
CucumberAdmin <CucumberAdmin@VSQVWDEVSECOP01.central-insurance.com>
2021-11-29 11:05:42 -0500

    ASP.net with ELK

---
+++ SampleWebApiAspNetCore/Controllers/v2/FoodsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace SampleWebApiAspNetCore.v2.Controllers
+{
+    [ApiController]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class FoodsController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()       
+        {
+            try
+            {
+                var rng = new Random();
+                    
+                if (rng.Next(0,5) < 2)
+                {
+                    throw new Exception("Oops what happened");
+                }                
+                return Ok("2.0");
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Error(ex, "Something bad happened...");      
+                return new StatusCodeResult(500);
+            }
+        }
+        
+        /*
+        {
+            return Ok("2.0");
+        }
+        */
+    }
+}
---
+++ SampleWebApiAspNetCore/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SampleWebApiAspNetCore.Repositories;
+using SampleWebApiAspNetCore.Services;
+using System;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+
+namespace SampleWebApiAspNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            // Initializes db.
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                
+                //var logger = services.GetRequiredService<ILogger<Program>>();
+                //logger.LogInformation("hey this is the request!");
+
+                try
+                {
+                    var context = services.GetRequiredService<FoodDbContext>();
+                    var dbInitializer = services.GetRequiredService<ISeedDataService>();
+                    dbInitializer.Initialize(context).GetAwaiter().GetResult();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+           Host.CreateDefaultBuilder(args)
+               
+            // Begin - Coding for SeriLog Logging
+               .UseSerilog(configureLogger:(context, configuration) => 
+               {
+                   configuration.Enrich.FromLogContext()
+                        .Enrich.WithMachineName()                
+                        .WriteTo.Console()
+                        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+                        {
+                            IndexFormat = $"{context.Configuration["ApplicationName"]}-logs-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
+
+                            AutoRegisterTemplate = true,
+                            NumberOfShards = 2,
+                            NumberOfReplicas = 1
+                        })
+                        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+                        .ReadFrom.Configuration(context.Configuration);
+
+               })
+                
+
+               // End - Coding for Serilog Logging 
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.UseStartup<Startup>();
+               });
+    }
+}
